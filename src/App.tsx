8b90b4671aec81f3b27cd487e017ccc034420239
Rodrigo Teixeira Ribeiro da Silva <rodrigoteix1998@gmail.com>
2024-07-12 11:25:57 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./globals.css"
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster } from 'sonner'
+
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet
+        titleTemplate="%s | PizzaDash"
+      />
+      <Toaster
+        closeButton
+      />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
+}
